6e3b38e9f819b984b1afc7ca1315129279a380bc
huangchao2528 <140641999+huangchao2528@users.noreply.github.com>
2024-08-26 21:52:53 -0400

    Delete RunWhisper.sh

--- RunWhisper.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Path to the audio file and output file
-input_file="/Users/erichuang/Documents/input.mp3"
-output_file="/Users/erichuang/Documents/output.srt"
-
-# Run the Whisper command with the specified model and output format
-whisper "$input_file" --model small --output_format srt --output_file "$output_file"
